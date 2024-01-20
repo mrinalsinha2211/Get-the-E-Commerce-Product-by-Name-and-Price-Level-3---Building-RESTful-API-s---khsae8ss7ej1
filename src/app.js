27ac1bb196159887e2d5975db0cfa8ffa1b9b198
mrinalsinha2211 <mrinalsinha53@gmail.com>
2024-01-20 10:59:28 +0530

    modifed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,28 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
 // Middlewares
 app.use(express.json());
-
+app.get("/products/:name/:price", (req,res)=>{
+const {name , price}=req.params;
+const product=products.find((product)=>product.name.toLowerCase()===name.toLowerCase() && product.price===  parseInt(price))
+if(product){                              
+    return res.status(200).json({
+        status: 'success',
+        message: 'Product fetched successfully',
+        data: {
+            product: {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            quantity: product.quantity,},
+        },
+    })
+}else{
+    return res.status(404).json({
+        status: "failed", 
+        message: "Product not found!",
+    })
+}
+})
 // Write GET endpoint for sending product to the client here
 // Endpoint - /api/v1/products/:name/:price
 
